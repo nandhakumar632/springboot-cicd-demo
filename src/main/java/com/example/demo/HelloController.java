c4034a61a335527dd9e08767dd05814501c31739
Nandhakumar <nandhakumar632@gmail.com>
2026-05-06 10:18:18 +0530

    changed...

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         System.out.println("hellooooooooooo");
-        return "Hello CI/CD/MY NAME IS NANDHA KUMAR";
+        return "hiiiii";
     }
 }
