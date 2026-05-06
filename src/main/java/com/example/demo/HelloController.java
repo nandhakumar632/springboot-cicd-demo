c9e61595a5771b6d1051fe31382a75fe5a1c1a1e
Nandhakumar <nandhakumar632@gmail.com>
2026-05-06 09:57:09 +0530

    Updated API and added Kubernetes deployment files

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        System.out.print("hellooooooooooo");
         return "Hello CI/CD/MY NAME IS NANDHA KUMAR";
     }
 }
